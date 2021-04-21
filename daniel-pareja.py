2bd49091fabc50a5e048d412544297e124f0e651
Daniel <danielpareja1992@gmail.com>
2021-04-21 12:56:34 +0200

    pulido

--- daniel-pareja.py
+++ daniel-pareja.py
@@ -1,8 +1,8 @@
-a=int(input("Primer número: "))
-b=int(input("Segundo número: "))
-if a==b:
+a = int(input("Primer número: "))
+b = int(input("Segundo número: "))
+if a == b:
     print("Iguales")
-elif a>b:
+elif a > b:
     print("Mayor")
 else:
     print("Menor")
